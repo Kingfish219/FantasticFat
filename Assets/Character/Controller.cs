6ecfabec12581a6c4295d4b2f14e51a1701faf5d
Kingfish <https.mohammad@gmail.com>
2024-01-25 18:10:34 +0330

    Fix player movement

--- Assets/Character/Controller.cs
+++ Assets/Character/Controller.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    [SerializeField] 
     public float speed = 2f;
+    [SerializeField] 
+    private float padding = 0.2f;
+
     private Rigidbody2D rd;
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
 
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();
         rd = GetComponent<Rigidbody2D>();
     }
 
@@ -17,17 +26,24 @@ public class Controller : MonoBehaviour
         Run();
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Wall", System.StringComparison.InvariantCultureIgnoreCase))
-        {
-            // Stop the sprite by setting its velocity to zero
-            rd.velocity = Vector3.zero;
-        }
-    }
-
     private void Run()
     {
-        transform.position += Vector3.right * speed * Time.deltaTime;
+        //transform.position += Vector3.right * speed * Time.deltaTime;
+
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
